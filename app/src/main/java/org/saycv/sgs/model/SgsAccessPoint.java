731cc30a6aba0c7b1044a326677115a446e82231
SayCV <sssxyc@163.com>
2014-12-11 16:19:36 +0800

    ...

---
+++ app/src/main/java/org/saycv/sgs/model/SgsAccessPoint.java
@@ -0,0 +1,158 @@
+/*
+ * Copyright (C) 2014, sayCV.
+ *
+ * Copyright 2014 The sayCV's Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.saycv.sgs.model;
+
+import org.saycv.sgs.services.impl.SgsNetworkService;
+import org.saycv.sgs.utils.SgsObservableObject;
+import org.saycv.sgs.utils.SgsStringUtils;
+
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+
+public class SgsAccessPoint extends SgsObservableObject {
+	// Constants used for different security types
+	public static final String AP_WPA2 = "WPA2";
+	public static final String AP_WPA = "WPA";
+	public static final String AP_WEP = "WEP";
+	public static final String AP_OPEN = "Open";
+	// For EAP Enterprise fields
+	public static final String AP_WPA_EAP = "WPA-EAP";
+	public static final String AP_IEEE8021X = "IEEE8021X";
+	public static final String[] AP_SECURITY_MODES = { AP_WEP, AP_WPA, AP_WPA2, AP_WPA_EAP, AP_IEEE8021X };
+
+	public static final int AP_WEP_PASSWORD_AUTO = 0;
+	public static final int AP_WEP_PASSWORD_ASCII = 1;
+	public static final int AP_WEP_PASSWORD_HEX = 2;
+	
+	private int mNetworkId;
+	private String mSSID;
+	private String mDescription;
+	private boolean mConnected;
+	private boolean mHasPassword;
+	private boolean mOpen;
+	private int mLevel;
+	private WifiConfiguration mConf;
+	private ScanResult mSR;
+	
+	public SgsAccessPoint(){
+		mNetworkId = -1;
+		mSSID = SgsStringUtils.emptyValue();
+		mLevel = 0;
+		mDescription = SgsStringUtils.emptyValue();
+	}
+	
+	public SgsAccessPoint(int networkId, String SSID){
+		this();
+		mNetworkId = networkId;
+		mSSID = SSID;
+	}
+	
+	public SgsAccessPoint(ScanResult sr){
+		this();
+		if((mSR = sr) != null){
+			mSSID = sr.SSID;
+			mLevel = WifiManager.calculateSignalLevel(mSR.level,
+					SgsNetworkService.sWifiSignalValues.length);
+			final String rs = getScanResultSecurity(mSR);
+			mOpen = SgsStringUtils.equals(rs, AP_OPEN, false);
+			mDescription = String.format("%s;%s", 
+					rs, 
+					mSR.capabilities);
+		}
+	}
+	
+	public SgsAccessPoint(WifiConfiguration conf){
+		this();
+		if((mConf = conf) != null){
+			mSSID = SgsStringUtils.unquote(mConf.SSID, "\"") ;
+			mNetworkId = conf.networkId;
+			mHasPassword = !SgsStringUtils.isNullOrEmpty(conf.preSharedKey);
+		}
+	}
+	
+	public WifiConfiguration getConf(){
+		return mConf;
+	}
+	
+	public ScanResult getSR(){
+		return mSR;
+	}
+	
+	public void setNetworkId(int networkId){
+		mNetworkId = networkId;
+	}
+	
+	public int getNetworkId(){
+		return mNetworkId;
+	}
+	
+	public String getSSID(){
+		return mSSID;
+	}
+	
+	public String getDescription(){
+		return mDescription;
+	}
+	
+	public int getLevel(){
+		return mLevel;
+	}
+	
+	public void setLevel(int level){
+		if(mLevel != level){
+			mLevel = level;
+			super.setChangedAndNotifyObservers(this);
+		}
+	}
+	
+	public boolean isConfigured(){
+		return (mNetworkId >= 0);
+	}
+	
+	public void setConnected(boolean connected){
+		if(mConnected != connected){
+			mConnected = connected;
+			super.setChangedAndNotifyObservers(this);
+		}
+	}
+	
+	public boolean isConnected(){
+		return mConnected;
+	}
+	
+	public boolean hasPassword(){
+		return mHasPassword;
+	}
+	
+	public boolean isOpen(){
+		return mOpen;
+	}
+	
+	public static String getScanResultSecurity(ScanResult scanResult) {
+		final String cap = scanResult.capabilities;
+		for (int i = SgsAccessPoint.AP_SECURITY_MODES.length - 1; i >= 0; i--) {
+			if (cap.contains(SgsAccessPoint.AP_SECURITY_MODES[i])) {
+				return SgsAccessPoint.AP_SECURITY_MODES[i];
+			}
+		}
+
+		return SgsAccessPoint.AP_OPEN;
+	}
+}
--- app/src/main/java/org/saycv/sgs/utils/SgsConfigurationEntry.java
+++ app/src/main/java/org/saycv/sgs/utils/SgsConfigurationEntry.java
@@ -1,33 +1,23 @@
-/* Copyright (C) 2010-2011, Mamadou Diop.
-*  Copyright (C) 2011, Doubango Telecom.
-*  Copyright (C) 2011, Philippe Verney <verney(dot)philippe(AT)gmail(dot)com>
-*
-* Contact: Mamadou Diop <diopmamadou(at)saycv(dot)org>
-*	
-* This file is part of imsdroid Project (http://code.google.com/p/imsdroid)
-*
-* imsdroid is free software: you can redistribute it and/or modify it under the terms of 
-* the GNU General Public License as published by the Free Software Foundation, either version 3 
-* of the License, or (at your option) any later version.
-*	
-* imsdroid is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
-* See the GNU General Public License for more details.
-*	
-* You should have received a copy of the GNU General Public License along 
-* with this program; if not, write to the Free Software Foundation, Inc., 
-* 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
+/*
+ * Copyright (C) 2014, sayCV.
+ *
+ * Copyright 2014 The sayCV's Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.saycv.sgs.utils;
 
 import org.saycv.sgs.sip.SgsPresenceStatus;
-import org.saycv.tinyWRAP.tdav_codec_id_t;
-import org.saycv.tinyWRAP.tmedia_bandwidth_level_t;
-import org.saycv.tinyWRAP.tmedia_pref_video_size_t;
-import org.saycv.tinyWRAP.tmedia_profile_t;
-import org.saycv.tinyWRAP.tmedia_qos_strength_t;
-import org.saycv.tinyWRAP.tmedia_qos_stype_t;
-import org.saycv.tinyWRAP.tmedia_srtp_mode_t;
 
 
 public class SgsConfigurationEntry {
@@ -63,215 +53,4 @@ public class SgsConfigurationEntry {
 	public static final String IDENTITY_PASSWORD = "IDENTITY_PASSWORD." + TAG;
 	
 	// Network
-	public static final String NETWORK_REGISTRATION_TIMEOUT = "NETWORK_REGISTRATION_TIMEOUT." + TAG;
-	public static final String NETWORK_REALM = "NETWORK_REALM." + TAG;
-	public static final String NETWORK_USE_WIFI = "NETWORK_USE_WIFI." + TAG;
-    public static final String NETWORK_USE_FAKED_3G = "NETWORK_USE_FAKED_3G." + TAG;
-	public static final String NETWORK_USE_3G = "NETWORK_USE_3G." + TAG;
-	public static final String NETWORK_USE_EARLY_IMS = "NETWORK_USE_EARLY_IMS." + TAG;
-	public static final String NETWORK_IP_VERSION = "NETWORK_IP_VERSION." + TAG;
-	public static final String NETWORK_PCSCF_DISCOVERY = "NETWORK_PCSCF_DISCOVERY." + TAG;
-	public static final String NETWORK_PCSCF_HOST = "NETWORK_PCSCF_HOST." + TAG;
-	public static final String NETWORK_PCSCF_PORT = "NETWORK_PCSCF_PORT." + TAG;
-	public static final String NETWORK_USE_SIGCOMP = "NETWORK_USE_SIGCOMP." + TAG;
-	public static final String NETWORK_TRANSPORT = "NETWORK_TRANSPORT." + TAG;
-
-    public static final String NETWORK_TRAFFIC_COUNT_THREAD_STATE = "NETWORK_TRAFFIC_COUNT_THREAD_STATE." + TAG;
-    public static final String NETWORK_TRAFFIC_COUNT_AT_START = "NETWORK_TRAFFIC_COUNT_AT_START." + TAG;
-    public static final String NETWORK_IP_STYLE = "NETWORK_IP_STYLE." + TAG;
-    public static final String NETWORK_CONNECTED = "NETWORK_CONNECTED." + TAG;
-    public static final String NETWORK_NOTIFICATION_RING_TONE = "NETWORK_NOTIFICATION_RING_TONE." + TAG;
-    public static final String NETWORK_LOCAL_IP = "NETWORK_LOCAL_IP." + TAG;
-    public static final String NETWORK_SUB_MASK = "NETWORK_SUB_MASK." + TAG;
-    public static final String NETWORK_GATE_WAY = "NETWORK_GATE_WAY." + TAG;
-    public static final String NETWORK_PREFERRED_DNS = "NETWORK_PREFERRED_DNS." + TAG;
-    public static final String NETWORK_SECONDARY_DNS = "NETWORK_SECONDARY_DNS." + TAG;
-
-    // Manual
-    public static final String MANUAL_ENABLE_USB_TETHER_CONNECT = "MANUAL_ENABLE_USB_TETHER_CONNECT." + TAG;
-    public static final String MANUAL_ENABLE_MOBILE_DATA_AND_FAKED = "MANUAL_ENABLE_MOBILE_DATA_AND_FAKED." + TAG;
-    public static final String MANUAL_ENABLE_AUTO_CONFIG_USB_TETHER_IP = "MANUAL_ENABLE_AUTO_CONFIG_USB_TETHER_IP." + TAG;
-
-	// NAT Traversal
-	public static final String NATT_HACK_AOR = "NATT_HACK_AOR." + TAG;
-	public static final String NATT_HACK_AOR_TIMEOUT = "NATT_HACK_AOR_TIMEOUT." + TAG;
-	public static final String NATT_USE_STUN = "NATT_USE_STUN." + TAG;
-	public static final String NATT_USE_ICE = "NATT_USE_ICE." + TAG;
-	public static final String NATT_STUN_DISCO = "NATT_STUN_DISCO." + TAG;
-	public static final String NATT_STUN_SERVER = "NATT_STUN_SERVER." + TAG;
-	public static final String NATT_STUN_PORT = "NATT_STUN_PORT." + TAG;
-	
-	// QoS
-	public static final String QOS_PRECOND_BANDWIDTH_LEVEL = "QOS_PRECOND_BANDWIDTH_LEVEL." + TAG;
-	public static final String QOS_PRECOND_STRENGTH = "QOS_PRECOND_STRENGTH." + TAG;
-    public static final String QOS_PRECOND_TYPE = "QOS_PRECOND_TYPE." + TAG;
-    public static final String QOS_REFRESHER = "QOS_REFRESHER." + TAG;
-    public static final String QOS_SIP_CALLS_TIMEOUT = "QOS_SIP_CALLS_TIMEOUT." + TAG;
-    public static final String QOS_SIP_SESSIONS_TIMEOUT = "QOS_SIP_SESSIONS_TIMEOUT" + TAG;
-    public static final String QOS_USE_SESSION_TIMERS = "QOS_USE_SESSION_TIMERS." + TAG;
-    public static final String QOS_PREF_VIDEO_SIZE = "QOS_PREF_VIDEO_SIZE." + TAG;
-
-	
-	// Media
-	public static final String MEDIA_CODECS = "MEDIA_CODECS." + TAG;
-	public static final String MEDIA_AUDIO_RESAMPLER_QUALITY = "MEDIA_AUDIO_RESAMPLER_QUALITY." + TAG;
-	public static final String MEDIA_AUDIO_CONSUMER_GAIN = "MEDIA_AUDIO_CONSUMER_GAIN." + TAG;
-	public static final String MEDIA_AUDIO_PRODUCER_GAIN = "MEDIA_AUDIO_PRODUCER_GAIN." + TAG;
-	public static final String MEDIA_AUDIO_CONSUMER_ATTENUATION = "MEDIA_AUDIO_CONSUMER_ATTENUATION." + TAG;
-	public static final String MEDIA_AUDIO_PRODUCER_ATTENUATION = "MEDIA_AUDIO_PRODUCER_ATTENUATION." + TAG;
-	public static final String MEDIA_PROFILE = "MEDIA_PROFILE." + TAG;
-	
-	// Security
-	public static final String SECURITY_SRTP_MODE = "SECURITY_SRTP_MODE." + TAG;
-	public static final String SECURITY_IMSAKA_AMF = "SECURITY_IMSAKA_AMF." + TAG;
-	public static final String SECURITY_IMSAKA_OPID = "SECURITY_IMSAKA_OPID." + TAG;
-	
-	// XCAP
-	public static final String XCAP_PASSWORD = "XCAP_PASSWORD." + TAG;
-	public static final String XCAP_USERNAME = "XCAP_USERNAME." + TAG;
-	public static final String XCAP_ENABLED = "XCAP_ENABLED." + TAG;
-	public static final String XCAP_XCAP_ROOT = "XCAP_XCAP_ROOT." + TAG;
-	
-	// RCS (Rich Communication Suite)
-	public static final String RCS_AVATAR_PATH = "RCS_AVATAR_PATH." + TAG;
-	public static final String RCS_USE_BINARY_SMS = "RCS_USE_BINARY_SMS." + TAG;
-	public static final String RCS_CONF_FACT = "RCS_CONF_FACT." + TAG;
-	public static final String RCS_FREE_TEXT = "RCS_FREE_TEXT." + TAG;
-	public static final String RCS_HACK_SMS = "RCS_HACK_SMS." + TAG;
-	public static final String RCS_USE_MSRP_FAILURE = "RCS_USE_MSRP_FAILURE." + TAG;
-	public static final String RCS_USE_MSRP_SUCCESS = "RCS_USE_MSRP_SUCCESS." + TAG;
-	public static final String RCS_USE_MWI = "RCS_USE_MWI." + TAG;
-	public static final String RCS_USE_OMAFDR = "RCS_USE_OMAFDR." + TAG;
-	public static final String RCS_USE_PARTIAL_PUB = "RCS_USE_PARTIAL_PUB." + TAG;
-	public static final String RCS_USE_PRESENCE = "RCS_USE_PRESENCE." + TAG;
-	public static final String RCS_USE_RLS = "RCS_USE_RLS." + TAG;
-	public static final String RCS_SMSC = "RCS_SMSC." + TAG;
-	public static final String RCS_STATUS  = "RCS_STATUS." + TAG;
-	
-	
-	//
-	//	Default values
-	//
-	
-	// General
-    public static final boolean DEFAULT_GENERAL_STARTUP_CHECK = true;
-	public static final boolean DEFAULT_GENERAL_SHOW_WELCOME_SCREEN = true;
-	public static final boolean DEFAULT_GENERAL_FULL_SCREEN_VIDEO = true;
-	public static final boolean DEFAULT_GENERAL_INTERCEPT_OUTGOING_CALLS = true;
-	public static final boolean DEFAULT_GENERAL_USE_FFC = true;
-	public static final boolean DEFAULT_GENERAL_AUTOSTART = true;
-	public static final float DEFAULT_GENERAL_AUDIO_PLAY_LEVEL = 0.25f;
-	public static final String DEFAULT_GENERAL_ENUM_DOMAIN = "e164.org";
-	public static final boolean DEFAULT_GENERAL_AEC = true;
-	public static final boolean DEFAULT_GENERAL_VAD = false; // speex-dsp don't support VAD for fixed-point implementation
-	public static final boolean DEFAULT_GENERAL_NR = true;
-	public static final int DEFAULT_GENERAL_ECHO_TAIL = 500;
-	public static final boolean DEFAULT_GENERAL_SEND_DEVICE_INFO = false;
-
-    public static final boolean DEFAULT_GENERAL_VOC = true;
-    public static final boolean DEFAULT_GENERAL_DUC = false;
-    public static final boolean DEFAULT_GENERAL_DWL = false;
-    public static final boolean DEFAULT_GENERAL_DSO = true;
-	
-	//	Identity
-	public static final String DEFAULT_IDENTITY_DISPLAY_NAME = "saycv";
-	public static final String DEFAULT_IDENTITY_IMPU = "sip:saycv@saycv.org";
-	public static final String DEFAULT_IDENTITY_IMPI = "saycv";
-	public static final String DEFAULT_IDENTITY_PASSWORD = null;
-	
-	// Network
-	public static final int DEFAULT_NETWORK_REGISTRATION_TIMEOUT = 1700;
-	public static final String DEFAULT_NETWORK_REALM = "saycv.org";
-	public static final boolean DEFAULT_NETWORK_USE_WIFI = false;
-	public static final boolean DEFAULT_NETWORK_USE_3G = true;
-    public static final boolean DEFAULT_NETWORK_USE_FAKED_3G = true;
-	public static final String DEFAULT_NETWORK_PCSCF_DISCOVERY = "None";
-	public static final String DEFAULT_NETWORK_PCSCF_HOST = "127.0.0.1";
-	public static final int DEFAULT_NETWORK_PCSCF_PORT = 5060;
-	public static final boolean DEFAULT_NETWORK_USE_SIGCOMP = false;
-	public static final String DEFAULT_NETWORK_TRANSPORT = "udp";
-	public static final String DEFAULT_NETWORK_IP_VERSION = "ipv4";
-	public static final boolean DEFAULT_NETWORK_USE_EARLY_IMS = false;
-
-    public static final int DEFAULT_NETWORK_TRAFFIC_COUNT_THREAD_STATE = 0;
-    public static final String DEFAULT_NETWORK_TRAFFIC_COUNT_AT_START = "";
-    public static final String DEFAULT_NETWORK_IP_STYLE = "ip_style_winxp";
-    public static final boolean DEFAULT_NETWORK_CONNECTED = false;
-    public static final String DEFAULT_NETWORK_NOTIFICATION_RING_TONE = "";
-    public static final String DEFAULT_NETWORK_LOCAL_IP_WINXP = "192.168.0.252";
-    public static final String DEFAULT_NETWORK_LOCAL_IP_WIN7 = "192.168.137.252";
-    public static final String DEFAULT_NETWORK_SUB_MASK = "255.255.255.0";
-    public static final String DEFAULT_NETWORK_GATE_WAY_WINXP = "192.168.0.1";
-    public static final String DEFAULT_NETWORK_GATE_WAY_WIN7 = "192.168.137.1";
-    public static final String DEFAULT_NETWORK_PREFERRED_DNS = "8.8.8.4";
-    public static final String DEFAULT_NETWORK_SECONDARY_DNS = "8.8.8.8";
-
-    // Manual
-    public static final boolean DEFAULT_MANUAL_ENABLE_USB_TETHER_CONNECT = false;
-    public static final boolean DEFAULT_MANUAL_ENABLE_MOBILE_DATA_AND_FAKED = false;
-    public static final boolean DEFAULT_MANUAL_ENABLE_AUTO_CONFIG_USB_TETHER_IP = false;
-	
-	// NAT Traversal
-	public static final int DEFAULT_NATT_HACK_AOR_TIMEOUT = 2000;
-	public static final boolean DEFAULT_NATT_HACK_AOR = false;
-	public static final boolean DEFAULT_NATT_USE_STUN = false;
-	public static final boolean DEFAULT_NATT_USE_ICE = false;
-	public static final boolean DEFAULT_NATT_STUN_DISCO = false;
-	public static final String DEFAULT_NATT_STUN_SERVER = "numb.viagenie.ca";
-	public static final int DEFAULT_NATT_STUN_PORT = 3478;
-	
-	// QoS
-    public static final int DEFAULT_QOS_PRECOND_BANDWIDTH_LEVEL = tmedia_bandwidth_level_t.tmedia_bl_unrestricted.swigValue(); // should be String but do not change for backward compatibility
-    public static final String DEFAULT_QOS_PRECOND_STRENGTH = tmedia_qos_strength_t.tmedia_qos_strength_none.toString();
-    public static final String DEFAULT_QOS_PRECOND_TYPE = tmedia_qos_stype_t.tmedia_qos_stype_none.toString();
-    public static final String DEFAULT_QOS_REFRESHER = "none";
-    public static final int DEFAULT_QOS_SIP_SESSIONS_TIMEOUT = 600000;
-    public static final int DEFAULT_QOS_SIP_CALLS_TIMEOUT = 3600;
-    public static final boolean DEFAULT_QOS_USE_SESSION_TIMERS = false;
-    public static final String DEFAULT_QOS_PREF_VIDEO_SIZE = tmedia_pref_video_size_t.tmedia_pref_video_size_cif.toString();
-	
-	// Media
-    public static final String DEFAULT_MEDIA_PROFILE = tmedia_profile_t.tmedia_profile_default.toString();
-	public static final int DEFAULT_MEDIA_CODECS = 
-		tdav_codec_id_t.tdav_codec_id_pcma.swigValue() |
-		tdav_codec_id_t.tdav_codec_id_pcmu.swigValue() |
-		
-		tdav_codec_id_t.tdav_codec_id_h264_bp.swigValue() |
-		tdav_codec_id_t.tdav_codec_id_vp8.swigValue() |
-		tdav_codec_id_t.tdav_codec_id_h263p.swigValue() |
-		tdav_codec_id_t.tdav_codec_id_h263.swigValue();
-	public static final int DEFAULT_MEDIA_AUDIO_RESAMPLER_QUALITY = 0;
-	public static final int DEFAULT_MEDIA_AUDIO_CONSUMER_GAIN = 0; // disabled
-	public static final int DEFAULT_MEDIA_AUDIO_PRODUCER_GAIN = 0; // disabled
-	public static final float DEFAULT_MEDIA_AUDIO_CONSUMER_ATTENUATION = 1f; // disabled
-	public static final float DEFAULT_MEDIA_AUDIO_PRODUCER_ATTENUATION = 1f; // disabled
-	
-	// Security
-	public static final String DEFAULT_SECURITY_IMSAKA_AMF = "0x0000";
-	public static final String DEFAULT_SECURITY_IMSAKA_OPID = "0x00000000000000000000000000000000";
-	public static final String DEFAULT_SECURITY_SRTP_MODE = tmedia_srtp_mode_t.tmedia_srtp_mode_none.toString();
-	
-	// XCAP
-	public static final boolean DEFAULT_XCAP_ENABLED = false;
-	public static final String DEFAULT_XCAP_ROOT = "http://saycv.org:8080/services";
-	public static final String DEFAULT_XCAP_USERNAME = "sip:johndoe@saycv.org";
-	public static final String DEFAULT_XCAP_PASSWORD = null;
-	
-	// RCS (Rich Communication Suite)
-	public static final String DEFAULT_RCS_AVATAR_PATH = "";
-	public static final boolean DEFAULT_RCS_USE_BINARY_SM = false; 
-	public static final String DEFAULT_RCS_CONF_FACT = "sip:Conference-Factory@saycv.org";
-	public static final String DEFAULT_RCS_FREE_TEXT = "Hello world";
-	public static final boolean DEFAULT_RCS_HACK_SMS = false;
-	public static final boolean DEFAULT_RCS_USE_MSRP_FAILURE = true;
-	public static final boolean DEFAULT_RCS_USE_MSRP_SUCCESS = false;
-	public static final boolean DEFAULT_RCS_USE_BINARY_SMS = false;
-	public static final boolean DEFAULT_RCS_USE_MWI = false;
-	public static final boolean DEFAULT_RCS_USE_OMAFDR = false;
-	public static final boolean DEFAULT_RCS_USE_PARTIAL_PUB = false;
-	public static final boolean DEFAULT_RCS_USE_PRESENCE = false;
-	public static final boolean DEFAULT_RCS_USE_RLS = false;
-	public static final String DEFAULT_RCS_SMSC = "sip:+331000000000@saycv.org";
-	public static final SgsPresenceStatus DEFAULT_RCS_STATUS = SgsPresenceStatus.Online;
-	
 }
