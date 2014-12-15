976373d0c2ce829187cd6f554075c65e9320bfc1
SayCV <sssxyc@163.com>
2014-12-15 09:08:39 +0800

    Addd *.bak to .gitignore file

--- app/src/main/java/com/example/saycv/stockartifact/MainActivity.java
+++ app/src/main/java/com/example/saycv/stockartifact/MainActivity.java
@@ -196,7 +196,7 @@ public class MainActivity extends SgsFragmentActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_home, container, false);
             return rootView;
         }
 
